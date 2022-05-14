e08e2542bc85c802656fc946d99c3d6d61a83a78
Santiago Espinosa Mooser <smooser@pm.me>
2022-05-14 14:59:39 +0800

    Got functions working properly. Added test cases

--- src/Translate.hs
+++ src/Translate.hs
@@ -14,22 +14,28 @@ import qualified Control.Arrow as Data.Bifunctor
 -- replace all occurences of x in body with var
 replace :: Sigma.Term -> Common.Var -> Sigma.Term -> Sigma.Term
 replace bod arg xself = case bod of
-    Sigma.Var y -> if arg == y then xself else Sigma.Var y
-    a -> a
+    Sigma.Var y                             -> if arg == y then Sigma.Invoke xself "arg" else Sigma.Var y
+    Sigma.Object mlist                      -> Sigma.Object [(l, Sigma.Method l (replace m arg xself)) | (l, Sigma.Method ml m) <- mlist]
+    Sigma.Invoke t l                        -> Sigma.Invoke (replace t arg xself) l
+    Sigma.Update t l (Sigma.Method ml m)    -> Sigma.Update (replace t arg xself) l (Sigma.Method ml (replace m arg xself))
+    Sigma.Clone t                           -> Sigma.Clone (replace t arg xself)
+    Sigma.Lit l                             -> Sigma.Lit l
+    Sigma.Unary op t                        -> Sigma.Unary op (replace t arg xself)
+    Sigma.Binary op t1 t2                   -> Sigma.Binary op (replace t1 arg xself) (replace t2 arg xself)
+    Sigma.If t1 t2 t3                       -> Sigma.If (replace t1 arg xself) (replace t2 arg xself) (replace t3 arg xself)
+    Sigma.Let x t1 t2                       -> Sigma.Let x (replace t1 arg xself) (replace t2 arg xself)
 
 
 translate :: Source.Exp -> Sigma.Term
 translate (Source.Var x) = Sigma.Var x
-
--- This is awefully convoluted but it's the only way I got it to work lmao
-translate (Source.Object mlist) =
-    -- Definte a mapping that takes a list of methods and applies translate to each
-    let t_body = map (\(l, Source.Method ml v) -> (l, Sigma.Method ml (translate v))) in --
-    case mlist of
-        -- If the list of methods is empty, just return an object with no methods (why? lol)
-        [] -> Sigma.Object []
-        -- Otherwise translate all the methods and then return an object with the translated methods
-        (l, Source.Method ml v):xs -> Sigma.Object ((l, Sigma.Method ml (translate v)):t_body xs)
+translate (Source.Object mlist) = Sigma.Object [(l, Sigma.Method ml (translate m)) | (l, Source.Method ml m) <- mlist]
+    -- Define a mapping that takes a list of methods and applies translate to each
+    -- let t_body = map (\(l, Source.Method ml v) -> (l, Sigma.Method ml (translate v))) in --
+    -- case mlist of
+    --     -- If the list of methods is empty, just return an object with no methods (why? lol)
+    --     [] -> Sigma.Object []
+    --     -- Otherwise translate all the methods and then return an object with the translated methods
+    --     (l, Source.Method ml v):xs -> Sigma.Object ((l, Sigma.Method ml (translate v)):t_body xs)
 translate (Source.Invoke exp label) = Sigma.Invoke (translate exp) label
 translate (Source.Update exp1 label (Source.Method v exp2)) = Sigma.Update (translate exp1) label (Sigma.Method v (translate exp2))
 translate (Source.Clone exp) = Sigma.Clone (translate exp)
@@ -43,7 +49,7 @@ translate (Source.Fun arg body) =
         xself           = Sigma.Var "xself"
         replaced_bod    = replace bod arg xself
     in Sigma.Object [
-            ("arg", Sigma.Method "xself" xself),
+            ("arg", Sigma.Method "xself" (Sigma.Invoke xself "arg")),
             ("val", Sigma.Method "xself" replaced_bod)
         ]
 translate (Source.Call func exp) =
@@ -55,7 +61,7 @@ translate (Source.New _) = error "translate: new"
 translate Source.Letrec {} = error "translate: letrec"
 translate (Source.Array _) = error "translate: array"
 --encode a function using an object with two methods. The first method (arg) stores the argument.
--- In the second method (val), body is first recursively trans-lated (denoted by |body|), and then all the occurrences of x are substituted by self.arg in the translated body
+-- In the second method (val), body is first recursively translated (denoted by |body|), and then all the occurrences of x are substituted by self.arg in the translated body
 -- translate (Source.Fun var exp) =
 
 -- translate (Source.Call exp exp) =S
